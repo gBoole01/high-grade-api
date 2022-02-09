7c80bcead2755b2cdba857ca083c173aa85130b8
Nicolas Turpin <nicolas@kaizen-developments.com>
2022-02-09 19:45:10 +0100

    feat: implements first middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import * as express from 'express';
 
+function loggerMiddleware(request: express.Request, response: express.Response, next: express.NextFunction) {
+    console.log(`${request.method} ${request.path}`);
+    next();
+}
+
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(loggerMiddleware);
+
+app.get('/hello', (request, response) => {
     response.send('Hello world!');
 });
 

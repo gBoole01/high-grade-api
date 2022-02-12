43f5e4ab3c7adcf6d57d547c082baf7808feb2cd
Nicolas Turpin <nicolas@kaizen-developments.com>
2022-02-12 17:25:32 +0100

    build: implements database connection to the app

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
+import { MongoClient } from 'mongodb';
+import dbConnectionURL from './config/db';
 import App from "./app";
 import PostsController from "./posts/posts.controller";
+
+const mongoClient = MongoClient;
+mongoClient.connect(dbConnectionURL, (err) => {
+    if (err) {
+        throw new Error('DB did not connect !'); // TODO => Create Exception
+    }
+    console.log('Database Succesfully connected !'); // TODO => Implement better logger
+});
 
 const app = new App(
     [
@@ -8,4 +18,4 @@ const app = new App(
     5000,
 );
 
-app.listen();
+app.listen(); // TODO => Chain this action with completion of mongoClient.connect

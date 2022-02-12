e722e172c7198d55004a037e456f0ab95f4dae83
Nicolas Turpin <nicolas@kaizen-developments.com>
2022-02-12 18:06:45 +0100

    feat: call .env validator on server init

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,8 @@
-import { MongoClient } from 'mongodb';
-import dbConnectionURL from './config/db';
 import App from "./app";
 import PostsController from "./posts/posts.controller";
+import validateEnv from './utils/validateEnv';
 
-const mongoClient = MongoClient;
-mongoClient.connect(dbConnectionURL, (err) => {
-    if (err) {
-        throw new Error('DB did not connect !'); // TODO => Create Exception
-    }
-    console.log('Database Succesfully connected !'); // TODO => Implement better logger
-});
+validateEnv();
 
 const app = new App(
     [

0d3ed58b8db9996154b78f9c17d07881147bdb52
Nicolas Turpin <nicolas@kaizen-developments.com>
2022-02-12 19:05:28 +0100

    feat: implements CRUD for Posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -7,6 +7,8 @@ class PostsController {
 
     public router = express.Router();
 
+    private postModel = postModel;
+
     constructor() {
         this.initializeRoutes();
     }
@@ -14,11 +16,13 @@ class PostsController {
     public initializeRoutes() {
         this.router.get(this.path, this.getAllPosts);
         this.router.get(`${this.path}/:id`, this.getPostById);
-        this.router.post(this.path, this.createAPost);
+        this.router.post(this.path, this.createPost);
+        this.router.patch(`${this.path}/:id`, this.updatePost);
+        this.router.delete(`${this.path}/:id`, this.deletePost);
     }
 
     getAllPosts = (_request: express.Request, response: express.Response) => {
-        postModel.find()
+        this.postModel.find()
             .then(posts => {
                 response.send(posts);
             });
@@ -26,18 +30,38 @@ class PostsController {
 
     getPostById = (request: express.Request, response: express.Response) => {
         const { id } = request.params;
-        postModel.findById(id)
+        this.postModel.findById(id)
             .then(post => {
                 response.send(post);
             });
     }
 
-    createAPost = (request: express.Request, response: express.Response) => {
+    createPost = (request: express.Request, response: express.Response) => {
         const postData: Post = request.body;
-        const createdPost = new postModel(postData);
+        const createdPost = new this.postModel(postData);
         createdPost.save()
             .then(savedPost => {
                 response.send(savedPost);
+            });
+    }
+
+    updatePost = (request: express.Request, response: express.Response) => {
+        const { id } = request.params;
+        const postData: Post = request.body;
+        this.postModel.findByIdAndUpdate(id, postData, { new: true })
+            .then(updatedPost => {
+                response.send(updatedPost);
+            });
+    }
+
+    deletePost = (request: express.Request, response: express.Response) => {
+        const { id } = request.params;
+        this.postModel.findByIdAndDelete(id)
+            .then(successResponse => {
+                if(successResponse) {
+                    response.sendStatus(200);
+                }
+                response.sendStatus(404);
             });
     }
 }

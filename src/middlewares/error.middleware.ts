30c27cce7b2ccb73df9aafbb978544873f4575c7
Nicolas Turpin <nicolas@kaizen-developments.com>
2022-02-12 19:38:43 +0100

    feat: implements 'error.middleware'

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,16 @@
+import * as express from "express";
+import { NextFunction } from "connect";
+import HttpException from "exceptions/HttpException";
+
+function errorMiddleware(error: HttpException, request: express.Request, response: express.Response, next: express.NextFunction) {
+    const status = error.status || 500;
+    const message = error.message || 'Something went wrong';
+    response
+        .status(status)
+        .send({
+            status,
+            message,
+        })
+}
+
+export default errorMiddleware;

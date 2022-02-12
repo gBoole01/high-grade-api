305aa822748529e2a6d259485b1bedcef03cce79
Nicolas Turpin <nicolas@kaizen-developments.com>
2022-02-12 18:07:30 +0100

    feat: implements database connection in App Class constructor

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+import dbConnectionURL from './config/db';
 
 class App {
     public app: express.Application;
     public port: number;
+    public mongoClient: any;
+    public dbConnectionURL: string;
 
     constructor(controllers, port) {
         this.app = express();
         this.port = port;
+        this.mongoClient = MongoClient;
+        this.dbConnectionURL = dbConnectionURL;
 
         this.initializeMiddlewares();
         this.initializeControllers(controllers);
+        this.connectToTheDatabase();
     }
 
     private initializeMiddlewares() {
@@ -20,6 +27,15 @@ class App {
     private initializeControllers(controllers) {
         controllers.forEach(controller => {
             this.app.use('/', controller.router);
+        });
+    }
+
+    private connectToTheDatabase(){
+        this.mongoClient.connect(this.dbConnectionURL, (err) => {
+            if (err) {
+                throw new Error('DB did not connect !'); // TODO => Create Exception
+            }
+            console.log('Database Succesfully connected !'); // TODO => Implement better logger
         });
     }
 

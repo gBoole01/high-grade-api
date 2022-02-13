b64351997b7431434bf09a30fe2579a18377d2da
Nicolas Turpin <nicolas@kaizen-developments.com>
2022-02-13 21:25:06 +0100

    feat: implement 'authentication.controller' with register and login functions

---
+++ src/authentication/authentication.controller.ts
@@ -0,0 +1,56 @@
+import * as bcrypt from 'bcrypt';
+import * as express from 'express';
+import Controller from '../interfaces/controller.interface';
+import validationMiddleware from '../middlewares/validation.middleware';
+import userModel from '../users/user.model';
+import CreateUserDto from '../users/user.dto';
+import LogInDto from './logIn.dto';
+import EmailAlreadyInUseException from '../exceptions/EmailAlreadyInUseException';
+import WrongCredentialsException from '../exceptions/WrongCredentialsException';
+
+class AuthenticationController implements Controller {
+    public path = 'auth';
+
+    public router = express.Router();
+
+    private user = userModel;
+
+    constructor() {
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes(){
+        this.router.post(`${this.path}/register`, validationMiddleware(CreateUserDto), this.registration);
+        this.router.post(`${this.path}/login`, validationMiddleware(LogInDto), this.loggingIn);
+    }
+
+    private registration = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
+        const userData: CreateUserDto = request.body;
+        if (await this.user.findOne({ email: userData.email })) {
+            next(new EmailAlreadyInUseException(userData.email));
+        }
+        const hashedPassword = await bcrypt.hash(userData.password, 10);
+        const user = await this.user.create({
+            ...userData,
+            password: hashedPassword,
+        });
+        user.password = undefined;
+        response.send(user);
+    }
+
+    private loggingIn = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
+        const logInData: LogInDto = request.body;
+        const user = await this.user.findOne({ email: logInData.email });
+        if (user) {
+            const isPasswordMatching = await bcrypt.compare(logInData.password, user.password);
+            if (isPasswordMatching) {
+                user.password = undefined;
+                response.send(user);
+            }
+            next(new WrongCredentialsException());
+        }
+        next(new WrongCredentialsException());
+    }
+}
+
+export default AuthenticationController;

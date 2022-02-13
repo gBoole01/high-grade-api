f66f6dc81581cd3728064be45913a689725b5be2
Nicolas Turpin <nicolas@kaizen-developments.com>
2022-02-13 13:29:16 +0100

    feat: implements Object validation middleware

---
+++ src/middlewares/validation.middleware.ts
@@ -0,0 +1,18 @@
+import * as express from 'express';
+import { plainToInstance } from 'class-transformer';
+import { validate, ValidationError } from 'class-validator';
+import HttpException from '../exceptions/HttpException';
+
+export default function validationMiddleware<T>(type:any): express.RequestHandler {
+    return (req, _res, next) => {
+        validate(plainToInstance(type, req.body))
+            .then((errors: ValidationError[]) => {
+                if (errors.length > 0) {
+                    const message = errors.map((error: ValidationError) =>
+                        Object.values(error.constraints)).join(', ');
+                    next(new HttpException(400, message));
+                }
+                next()
+            })
+    }
+}

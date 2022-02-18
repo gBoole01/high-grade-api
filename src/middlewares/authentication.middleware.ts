3b29f3a999bc59a49f5521c1a5755ff206af60ec
Nicolas Turpin <nicolas@kaizen-developments.com>
2022-02-18 21:34:02 +0100

    implements 'authentication.middleware'

---
+++ src/middlewares/authentication.middleware.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Response } from "express";
+import * as jwt from 'jsonwebtoken';
+import RequestWithUser from "../interfaces/RequestWithUser.interface";
+import DataStoredInAuthenticationToken from "../interfaces/DataStoredInAuthenticationToken.interface";
+import userModel from "../users/user.model";
+import WrongAuthenticationTokenException from "../exceptions/WrongAuthenticationTokenException";
+
+async function authenticationMiddleware(request: RequestWithUser, response: Response, next: NextFunction) {
+    const { cookies } = request
+    if (cookies && cookies.Authorization) {
+        const secret = process.env.JWT_SECRET_KEY;
+        try {
+            const verifcationResponse = jwt.verify(cookies.Authorization, secret) as DataStoredInAuthenticationToken;
+            const { _id } = verifcationResponse;
+            const user = await userModel.findById(_id);
+            if (user) {
+                request.user = user;
+                next();
+            } else {
+                next(new WrongAuthenticationTokenException());
+            }
+        } catch(error) {
+            next(new WrongAuthenticationTokenException());
+        }
+    } else {
+        next(new WrongAuthenticationTokenException());
+    }
+}
+
+export default authenticationMiddleware;

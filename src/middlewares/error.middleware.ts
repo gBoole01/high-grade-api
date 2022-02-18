1eb25a880fdcf554f8d279c163159223829346d3
Nicolas Turpin <nicolas@kaizen-developments.com>
2022-02-18 22:23:01 +0100

    style: some little improvements

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import { NextFunction } from "connect";
 import HttpException from "exceptions/HttpException";
 
-function errorMiddleware(error: HttpException, request: express.Request, response: express.Response, next: express.NextFunction) {
+function errorMiddleware(error: HttpException, _request: express.Request, response: express.Response, next: express.NextFunction) {
     const status = error.status || 500;
     const message = error.message || 'Something went wrong';
     response

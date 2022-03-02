5486a65abf3d04e575f609bf28a3c9982ec65337
gBoole01 <ncls4595@tuta.io>
2022-03-02 16:29:46 +0100

    build: add prettier and eslint-airbnb-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,17 @@
 module.exports = {
-    env: {
-        es2021: true,
-        node: true,
-    },
+    root: true,
     parser: '@typescript-eslint/parser',
     plugins: [
-        '@typescript-eslint'
+        '@typescript-eslint',
     ],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'airbnb-base',
+        'airbnb-typescript/base',
+        'prettier',
     ],
     parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
         project: ['./tsconfig.json'],
     },
     rules: {

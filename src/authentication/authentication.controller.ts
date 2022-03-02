0e5f064b4d3364b46f8ea185c75687002da0bdec
gBoole01 <ncls4595@tuta.io>
2022-03-02 09:46:40 +0100

    refactor: remove functions implemented in 'authentication.service'

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,13 +1,9 @@
 import * as bcrypt from 'bcrypt';
 import * as express from 'express';
-import * as jwt from 'jsonwebtoken';
 import Controller from '../interfaces/Controller.interface';
 import AuthenticationService from './authentication.service';
 import userModel from '../users/user.model';
 import validationMiddleware from '../middlewares/validation.middleware';
-import AuthenticationTokenData from 'interfaces/AuthenticationTokenData.interface';
-import DataStoredInAuthenticationToken from 'interfaces/DataStoredInAuthenticationToken.interface';
-import User from 'interfaces//User.interface';
 import CreateUserDto from '../users/user.dto';
 import LogInDto from './logIn.dto';
 import WrongCredentialsException from '../exceptions/WrongCredentialsException';
@@ -49,10 +45,13 @@ export default class AuthenticationController implements Controller {
         const logInData: LogInDto = request.body;
         const user = await this.user.findOne({ email: logInData.email });
         if (user) {
-            const isPasswordMatching = await bcrypt.compare(logInData.password, user.get('password', null, { getters: false }),);
+            const isPasswordMatching = await bcrypt.compare(
+                logInData.password,
+                user.get('password', null, { getters: false }),
+            );
             if (isPasswordMatching) {
-                const tokenData = this.createToken(user);
-                response.setHeader('Set-Cookie', [this.createCookie(tokenData)]);
+                const tokenData = this.authenticationService.createToken(user);
+                response.setHeader('Set-Cookie', [this.authenticationService.createCookie(tokenData)]);
                 response.send(user);
             } else {
                 next(new WrongCredentialsException());
@@ -65,21 +64,5 @@ export default class AuthenticationController implements Controller {
     private loggingOut = (_request: express.Request, response: express.Response) => {
         response.setHeader('Set-Cookie', ['Authorizatio;Max-age=0']);
         response.sendStatus(200);
-    }
-
-    private createCookie(tokenData: AuthenticationTokenData) {
-        return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn}`;
-    }
-
-    private createToken(user: User): AuthenticationTokenData {
-        const expiresIn = 60 * 60;
-        const secret = process.env.JWT_SECRET_KEY;
-        const dataStoredInToken: DataStoredInAuthenticationToken = {
-            _id: user._id,
-        };
-        return {
-            expiresIn,
-            token: jwt.sign(dataStoredInToken, secret, { expiresIn }),
-        };
     }
 }
